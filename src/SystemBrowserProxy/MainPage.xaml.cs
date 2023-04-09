b99dd0dd5b8ea52f8be63d2bb9b436ed9d1c6878
maxim-shlain <maxshlain@gmail.com>
2023-04-09 21:28:09 +0300

    add serilog

--- src/SystemBrowserProxy/MainPage.xaml.cs
+++ src/SystemBrowserProxy/MainPage.xaml.cs
@@ -1,40 +1,51 @@
-﻿using System.Diagnostics;
-using System.Threading;
+﻿using Serilog;
+using System.Diagnostics;
 
 namespace SystemBrowserProxy2;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-		CentralBanner.Text = GetCentralBannerText(); 
+    public MainPage()
+    {
+        InitializeComponent();
+        CentralBanner.Text = GetCentralBannerText(); 
     }
 
     private string GetCentralBannerText()
     {
-		Debugger.Launch();
+        //Debugger.Launch();
         var args = Environment.GetCommandLineArgs();
         string secondOrNull = args?.ElementAtOrDefault(1);
-		var path = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";
-		Process.Start(path, secondOrNull);
+        var path = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";
+        Process.Start(path, secondOrNull);
         string message = $"Starting msedge to ${secondOrNull}";
         //Environment.Exit(0);
-        return secondOrNull ?? "From Code Behind1";
+        var bannerText = secondOrNull ?? "From Code Behind1";
+        Serilog.Log.Information("{bannerText}", bannerText);
+        return bannerText;
     }
 
     private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    {
+        count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        Log.Logger.Information("counter: {counter}", count);
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 }
 
+public class CommandLineArgumentsProvider
+{
+    public static string[] GetCommandLineArguments()
+    {
+        var args = Environment.GetCommandLineArgs();
+        return args?.ToArray() ?? new string[0];
+    }
+}
--- src/SystemBrowserProxy/MauiProgram.cs
+++ src/SystemBrowserProxy/MauiProgram.cs
@@ -1,24 +1,60 @@
-﻿using Microsoft.Extensions.Logging;
+﻿
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace SystemBrowserProxy2;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
+    public static MauiApp CreateMauiApp()
+    {
+        SetupSerilog();
+
+        try
+        {
+            return CreateMauiAppImpl();
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, "Error while creating Maui App");
+            throw;
+        }
+        finally
+        {
+            //Log.CloseAndFlush();
+        }
+    }
+
+    private static MauiApp CreateMauiAppImpl()
+    {
+        MauiAppBuilder builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
+    
+        builder.Logging.AddSerilog(dispose: true);
+        return builder.Build();
+    }
+    
+    private static void SetupSerilog()
+    {
+        //var flushInterval = new TimeSpan(0, 0, 1);
+        var file = Path.Combine(FileSystem.AppDataDirectory, "MyApp.log");
 
-		return builder.Build();
-	}
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.File(file, encoding: System.Text.Encoding.UTF8, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 99)
+            .CreateLogger();
+    }
 }
